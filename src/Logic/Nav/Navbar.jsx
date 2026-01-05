18f9118a8fb507e51f894e337aeffc28d3dc83b5
Faja190 <fatimaajabukar@gmail.com>
2026-01-05 14:20:30 -0600

    fix missing hamburger
     fix the missing hamburger and add functionality of clisking anywhere to close menu as well as when on another page

--- src/Logic/Nav/Navbar.jsx
+++ src/Logic/Nav/Navbar.jsx
@@ -1,12 +1,41 @@
 import styles from "../../Styles/Navbar.module.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
 import { scroller } from "react-scroll";
 
+import { IoMenu } from "react-icons/io5";
+
 import resume from "../../resources/Fatima-resume.pdf";
+
 
 export default function Navbar() {
   const location = useLocation();
   const navigate = useNavigate();
+  const navRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        menuOpen &&
+        navRef.current &&
+        !navRef.current.contains(event.target)
+      ) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuOpen]);
+
+  //close menu if on a different page
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location.pathname]);
 
   const handleHomeClick = (e) => {
     e.preventDefault();
@@ -29,12 +58,23 @@ export default function Navbar() {
   };
 
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbar} ref={navRef}>
       <div className={styles.navbarLogo}>
         <a onClick={handleHomeClick}>F.Aja</a>
       </div>
 
-      <ul className={styles.navbarLinks}>
+      <button
+        className={styles.hamburger}
+        onClick={() => setMenuOpen(prev => !prev)}
+        aria-label="Menu"
+      >
+        <IoMenu />
+      </button>
+
+      <ul className={`${styles.navbarLinks} ${
+          menuOpen ? styles.showMenu : ""
+          }`} 
+      >
         <li>
           <a onClick={handleHomeClick}>Home</a>
         </li>
